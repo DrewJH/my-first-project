260c2512cfca824136bdef09376061f74a9ffaad
Drew <eukaryotic_drew@yahoo.com>
2019-08-02 08:37:47 -0500

    starbucks main reworked

--- starbucks/api.py
+++ starbucks/api.py
@@ -86,77 +86,64 @@ DRINKS = [
 	}
 ]
 
+DRINK_PICKER = [
+"Dark Roast",
+"Blonde Roast",
+"Espresso",
+"Caffe Latte",
+"Cappuccino"
+]
+
+SHOT_PICKER = [
+"Single",
+"Double",
+"Triple",
+"Quad"
+]
+
+SIZE_PICKER = [
+"Tall",
+"Grande",
+"Venti"
+]
+
 class Drink:
 	def __init__(self):
 		self.name = None
-		self.option = None
-		self.option2 = None
-		self.index = None
-		self.index2 = None
+		self.drink_menu = None
+		self.drink_selection = None
 		self.drink = None
+		self.type = None
+		self.size_menu = None
+		self.size_selection = None
+		self.shot_selection = None
 		self.size = None
 		self.price = None
-		self.drinks = [
-		"Dark Roast",
-		"Blonde Roast",
-		"Espresso",
-		"Caffe Latte",
-		"Cappuccino"
-		]
-		self.size_list = [
-		"Tall",
-		"Grande",
-		"Venti"
-		]
-		self.shots = [
-		"Single",
-		"Double",
-		"Triple",
-		"Quad"
-		]
-		self.price_brew = [
-			1.85,
-			2.10,
-			2.45
-		]
-		self.price_esp = [
-			1.75,
-			1.95,
-			2.25,
-			2.65
-		]
-		self.price_lc = [
-			2.95,
-			3.65,
-			4.15
-		]
 
 	def welcome(self):
 		self.name = input("Welcome to PythonBucks, please enter a name for your order: ")
 		return
 
 	def select_drink(self):
-		self.option, self.index = pick(self.drinks, f"Alright {self.name}, what'll it be?")
+		self.drink_menu, self.drink_selection = pick(DRINK_PICKER, f"Alright {self.name}, what'll it be?")
 		return
 
 	def select_size(self):
-		self.drink = self.drinks[self.index]
+		self.drink = DRINKS[self.drink_selection]['name']
+		self.type = DRINKS[self.drink_selection]['type']
 		if self.drink == "Espresso":
-			self.option2, self.index2 = pick(self.shots, "Espresso?  Excellent choice.  How many shots would you like?")
+			self.size_menu, self.size_selection = pick(SHOT_PICKER, "Espresso?  Excellent choice.  How many shots would you like?")
 		else:
-			self.option2, self.index2 = pick(self.size_list, f"Okay, you'd like a {self.drink}.  What size would you like?")
+			self.size_menu, self.size_selection = pick(SIZE_PICKER, f"Okay, you'd like a {self.drink}.  What size would you like?")
 		return
 
 	def get_price(self):
 		if self.drink == "Espresso":
-			self.size = self.shots[self.index2]
-			self.price = self.price_esp[self.index2]
-		elif self.drink == "Dark Roast" or "Blonde Roast":
-			self.size = self.size_list[self.index2]
-			self.price = self.price_brew[self.index2]
+			self.size = SHOTS[self.size_selection]['name']
+			self.price = PRICES[self.type][self.size_selection]
 		else:
-			self.size = self.size_list[self.index2]
-			self.price = self.price_lc[self.index2]
+			self.size = SIZES[self.size_selection]['name']
+			self.price = PRICES[self.type][self.size_selection]
 		print(f"{self.name}, your {self.size} {self.drink} will run you ${self.price}.")
 		return
 
--- starbucks/menu.py
+++ starbucks/menu.py
@@ -1,30 +1,30 @@
-dark = {
+{
 	"Tall" : 1.85,
 	"Grande" : 2.10,
 	"Venti" : 2.45
 }
 
-blonde = {
+{
 	"Tall" : 1.85,
 	"Grande" : 2.10,
 	"Venti" : 2.45
 }
 
-latte = {
-	"Tall" : 2.95,
-	"Grande" : 3.65,
-	"Venti" : 4.15
-}
-
-cap = {
-	"Tall" : 2.95,
-	"Grande" : 3.65,
-	"Venti" : 4.15
-}
-
-esp = {
+{
 	"Single" : 1.75, 
 	"Double" : 1.95, 
 	"Triple" : 2.25, 
 	"Quad" : 2.65
+}
+
+{
+	"Tall" : 2.95,
+	"Grande" : 3.65,
+	"Venti" : 4.15
+}
+
+{
+	"Tall" : 2.95,
+	"Grande" : 3.65,
+	"Venti" : 4.15
 }
